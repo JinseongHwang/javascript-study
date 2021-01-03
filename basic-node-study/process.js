37e6a0f2552173f8659aaa2a19ce49ae51990912
JinseongHwang <eddy5360@naver.com>
2021-01-03 14:33:23 +0900

    feat: process APIs

---
+++ basic-node-study/process.js
@@ -0,0 +1,30 @@
+
+// Enter in REPL
+
+// node version =>
+process.version
+
+// architecture info =>
+process.arch
+
+// os info =>
+process.platform
+
+// process ID =>
+process.pid
+
+// passed time =>
+process.uptime()
+
+// current working directory =>
+process.cwd()
+
+// usage of CPU =>
+process.cpuUsage()
+
+// system environment variables =>
+process.env
+
+// exit process =>
+process.exit(0); // normal situation
+process.exit(1); // abnormal situation
